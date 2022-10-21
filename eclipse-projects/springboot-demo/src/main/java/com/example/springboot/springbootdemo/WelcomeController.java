8097053ac2c30f0e38f088a776471735318f041b
deepakraghavendrabhat <deepakbhat.r@gmail.com>
2022-10-21 22:47:46 +0530

    Changes to spring project pom

--- eclipse-projects/springboot-demo/src/main/java/com/example/springboot/springbootdemo/WelcomeController.java
+++ eclipse-projects/springboot-demo/src/main/java/com/example/springboot/springbootdemo/WelcomeController.java
@@ -1,15 +1,26 @@
 package com.example.springboot.springbootdemo;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 @RestController
 public class WelcomeController {
 	
-	@GetMapping("/welcome")
+	/*@GetMapping("/welcome")
 	public String welcome() {
 		return "Welcome to Muddu Spring Boot WS";
 		
-	}
+	} */
+	@GetMapping("/hello")
+	public ModelAndView showMessage(
+			@RequestParam(value = "name", required = false, defaultValue = "World") String name) {
+
+			   ModelAndView mv = new ModelAndView("hello");
+			   mv.addObject("name", name);
+			   return mv;
+
+			}
 
 }
