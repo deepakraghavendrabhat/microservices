5a6b13fe66e170acddded22c5d289c461932c253
deepakraghavendrabhat <deepakbhat.r@gmail.com>
2022-10-28 21:27:52 +0530

    added docker project for spring boot
    implementation of docker file for spring boot demo project

---
+++ eclipse-projects/SpringDemoDocker/src/test/java/org/telco/billing/demo/SpringDemoDocker/SpringDemoDockerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.telco.billing.demo.SpringDemoDocker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDemoDockerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
